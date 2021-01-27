11d0de0b3ea80a35798cbe7f2676998c98e55f4d
Koc Grebeniuk <kgrebeniuk@gmail.com>
2021-01-27 21:10:00 +0200

    first add

---
+++ src/com/alfa/lesson1/TestScanner.java
@@ -0,0 +1,14 @@
+package com.alfa.lesson1;
+
+
+public class TestScanner {
+
+    public static D2501 fortest = new D2501("newtext");
+
+    public static void main(String[] args) {
+        fortest.input("Privet!");
+    }
+
+
+
+}
